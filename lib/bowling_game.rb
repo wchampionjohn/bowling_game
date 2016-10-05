40c5ba48681ebd1bb9294a779e47dd62892c5307
john.hsieh <john.hsieh@rockmobile.com>
2016-10-05 17:55:54 +0800

    [refactor] extra methods for current frame and times index

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,27 +1,24 @@
 class Game
   def initialize
-    @rolls = [[nil, nil]] * 10
+    @rolls = 10.times.inject([]) { |rolls| rolls << [nil, nil] }
   end
   def roll points
-    @rolls.each_with_index do |frame, frame_index|
-      if frame.include? nil
-        current_frame_index = frame_index
-
-        frame.each_with_index do |times, times_index|
-          if times.nil?
-            current_times_index = times_index  if times.nil?
-            @rolls[current_frame_index][current_times_index] = points
-            break
-          end
-        end
-        break
-      end
-    end
+    @rolls[current_frame_index][current_times_index] = points
   end
 
   def scope
     @rolls.inject(0) do |sum, frame|
       sum += frame.reduce(:+)
     end
+  end
+
+  def current_frame_index
+    @rolls.each_with_index do |frame, index|
+      return index if (frame.include?(nil)) && (frame.first != 10)
+    end
+  end
+
+  def current_times_index
+    @rolls[current_frame_index].index(nil)
   end
 end
