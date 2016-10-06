f008f505d1be79078227ea06ae6d103286434f7f
john.hsieh <john.hsieh@rockmobile.com>
2016-10-06 17:18:49 +0800

    refactor rename

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,15 +1,15 @@
 class Game
   def initialize
-    @rolls = 10.times.inject([]) { |rolls| rolls << [nil, nil] }
+    @frames = 10.times.inject([]) { |frames| frames << [nil, nil] }
   end
 
   def roll points
-    if current_frame_index == 9
-      @rolls << [nil, nil] if points == 10
-      @rolls << [nil] if (@rolls[current_frame_index].first.to_i + points == 10)
+    if current_frame_index == 9 # 第十局strike或space額外增加局數
+      @frames << [nil, nil] if points == 10
+      @frames << [nil] if (@frames[current_frame_index].first.to_i + points == 10)
     end
 
-    @rolls[current_frame_index][current_times_index] = points
+    @frames[current_frame_index][current_times_index] = points
   end
 
   def scope
@@ -17,8 +17,8 @@ class Game
       sum += frame.compact.reduce(:+)
 
       sum += if strike? frame
-               after_rolls = completed_rolls[index+1..index+2].flatten # 下兩局的分數
-               after_rolls[0].to_i + after_rolls[1].to_i # 下兩次的總和
+               next_rolls = completed_rolls[index+1..index+2].flatten # 下兩局的擊球分數
+               next_rolls[0].to_i + next_rolls[1].to_i # 下兩次擊球的總和
              elsif space? frame
                completed_rolls[index + 1].first
              else
@@ -29,21 +29,23 @@ class Game
 
   private
   def current_frame_index
-    @rolls.each_with_index do |frame, index|
-      return index if (frame.include?(nil)) && (frame.first != 10)
+    @frames.each_with_index do |frame, index|
+      return index if frame.include?(nil) && (frame.first != 10)
     end
   end
 
   def current_times_index
-    @rolls[current_frame_index].index(nil)
+    @frames[current_frame_index].index(nil)
   end
 
+  # 有擊球局數
   def completed_rolls
-    @rolls.each_with_object([]) do |frame, result|
+    @frames.each_with_object([]) do |frame, result|
       result << frame unless frame.compact.empty?
     end
   end
 
+  # 正規10局有擊球局數
   def regular_frames
     completed_rolls[0..9]
   end
