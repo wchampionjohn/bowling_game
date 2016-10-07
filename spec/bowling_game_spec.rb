c36896ef43abc2ae2ab5c326bb2a5f1f1b1f705f
john.hsieh <john.hsieh@rockmobile.com>
2016-10-07 15:37:53 +0800

    [refactor] extra methods

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -3,49 +3,43 @@ require_relative '../lib/bowling_game'
 require 'byebug'
 
 describe 'Bowling Game' do
+  let(:game) { Game.new }
   it 'can create game' do
-    Game.new
   end
 
   it 'can roll' do
-    game = Game.new
-    game.roll 10
+    game.roll 6
   end
 
   it 'can calculate scope' do
-    game = Game.new
     20.times { game.roll 3 }
     expect(game.scope).to eq 60
   end
 
-  it 'can calculate spacel' do
-    game = Game.new
+  it 'can extra append points if roll spacel' do
     game.roll 9
     game.roll 1
     game.roll 5
-    expect(game.scope).to eq 20
+    expect(game.scope).to eq 20 # 15 + 5
   end
 
-  it 'can calculate strike' do
-    game = Game.new
+  it 'can extra append points if roll strike' do
     game.roll 10
     game.roll 5
     game.roll 4
-    expect(game.scope).to eq 28
+    expect(game.scope).to eq 28 # 19 + 9
   end
 
-  it 'ofter last frame can add two times if last frame is strike' do
-    game = Game.new
+  it 'can extra append two times roll if last frame is strike' do
     18.times { game.roll 4 }
     game.roll 10
     game.roll 5
     game.roll 4
-    expect(game.scope).to eq 91
+    expect(game.scope).to eq 91 # 72 + 19
   end
 
 
-  it 'ofter last frame can add two times if last frame is strike' do
-    game = Game.new
+  it 'can extra append one times roll if last frame is space' do
     18.times { game.roll 4 }
     game.roll 5
     game.roll 5
@@ -53,15 +47,13 @@ describe 'Bowling Game' do
     expect(game.scope).to eq 86 # 72 + 14
   end
 
-  it 'can roll Turkey' do
-    game = Game.new
+  it 'can roll a Turkey' do
     3.times { game.roll 10 }
     2.times { game.roll 1 }
     expect(game.scope).to eq 65 # 30 + 21 + 12 + 2
   end
 
   it 'can roll a perfact game' do
-    game = Game.new
     12.times { game.roll 10 }
     expect(game.scope).to eq 300
   end
